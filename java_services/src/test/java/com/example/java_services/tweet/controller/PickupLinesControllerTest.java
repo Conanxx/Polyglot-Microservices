a1d4e1d567464bb7424def3f6c99601837ea803c
nahkg <dk.2401@yahoo.com>
2020-05-24 15:26:11 +0700

    Add test for PickupLinesController

--- java_services/src/test/java/com/example/java_services/tweet/controller/PickupLinesControllerTest.java
+++ java_services/src/test/java/com/example/java_services/tweet/controller/PickupLinesControllerTest.java
@@ -1,12 +1,42 @@
 package com.example.java_services.tweet.controller;
 
+import com.example.java_services.tweet.service.PickupLinesService;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
-
+@RunWith(SpringRunner.class)
+@WebMvcTest(PickupLinesController.class)
 public class PickupLinesControllerTest {
 
+    @MockBean
+    private PickupLinesService pkUpLinesService;
+
+    @Autowired
+    private MockMvc mvc;
+    
     @Before
     public void setUp() throws Exception {
     }
+
+    @Test
+    public void getRandomPickupLinesTest() throws Exception{
+
+        // Given some fake behavior of service ...
+        
+        // given(pkupLinesService.getPickupLines()).willReturn()
+
+        // when users access some url ...
+        
+        // MockHttpServletResponse response = mvc.perform()
+
+        // Then : assert that fake service  get called ...
+
+    }
+    
 }
